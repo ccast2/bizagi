6fc3adfc0b6d074b6eeeeefe9c31555be0bdc246
Carlos A. Castellanos <ccast2@gmail.com>
2017-02-02 08:32:52 -0500

    search with list

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,50 +1,58 @@
 (function() {
-  	app.controller('homeController', ["$scope","$http",function($scope,$http) {
-      
-        $scope.Movies = [];
-        $scope.MoviesDB = [];
-        $http.get("json/jsonmovies.json").then(function(r) {
-            $scope.MoviesDB = r.data.Movies;
-        });
+ app.controller('homeController', ["$scope","$http",function($scope,$http) {
 
-        $scope.keyword = '';
-        $scope.searchCriteria = {};
-        $scope.searchKeys = [
-        {
-            key:"Title",
-            label:"Titulo"
-        },
-        {
-            key:"Genre",
-            label:"Genero"
-        },
-        {
-            key:"Year",
-            label:"Año"
+    $scope.Movies = [];
+    $scope.MoviesDB = [];
+    $http.get("json/jsonmovies.json").then(function(r) {
+        $scope.MoviesDB = r.data.Movies;
+        $scope.Movies = $scope.MoviesDB;
+    });
+
+    $scope.keyword = '';
+    $scope.searchCriteria = {};
+    $scope.searchKeys = [
+    {
+        key:"Title",
+        label:"Titulo"
+    },
+    {
+        key:"Genre",
+        label:"Genero"
+    },
+    {
+        key:"Year",
+        label:"Año"
+    }
+    ];
+
+    $scope.validateSearch = function(newVal) {
+
+        if (!newVal) {
+            $scope.Movies = $scope.MoviesDB;
+        }else if (newVal.length > 2){
+            $scope.search(newVal);
         }
-        ]
+    }
 
-        $scope.$watch('keyword',function(newVal) {
+    $scope.$watch('keyword',$scope.validateSearch);
+    $scope.$watch('searchCriteria.key',function(newVal) {
+        console.log(newVal);
+        $scope.validateSearch($scope.keyword);
+    });
 
-            if (!newVal) {
-                $scope.Movies = $scope.MoviesDB;
-            }else{
-                $scope.search(newVal);
+    $scope.search = function(keyword) {
+        var searchKey = $scope.searchCriteria.key;
+        $scope.Movies = [];
+        angular.forEach($scope.MoviesDB, function(movie, key) {
+            for(prop in movie){
+                console.log(prop);
+                if (prop == searchKey && movie[prop].toLowerCase().includes(keyword.toLowerCase())) {
+                    $scope.Movies.push(movie);
+                }
             }
         });
+    }
 
-        $scope.search = function(keyword) {
-            var searchKey = $scope.searchCriteria.key;
-            angular.forEach($scope.MoviesDB, function(movie, key) {
-                for(prop in movie){
-                    console.log(prop);
-                    if (prop == searchKey) {
-
-                    }
-                }
-            });
-        }
-
-	}]);
+}]);
 }).call(this);
 
